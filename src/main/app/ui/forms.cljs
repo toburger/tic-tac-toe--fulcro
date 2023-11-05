809f6c7517650422358e241614b1802d1c0aa08d
Tobias Burger <tobias_burger@hotmail.com>
2023-11-05 10:49:40 +0100

    Add form info

--- src/main/app/ui/forms.cljs
+++ src/main/app/ui/forms.cljs
@@ -1,11 +1,12 @@
 (ns app.ui.forms
-  (:require [com.fulcrologic.fulcro.algorithms.form-state :as fs]
+  (:require [clojure.pprint :as pprint]
+            [com.fulcrologic.fulcro.algorithms.form-state :as fs]
             [com.fulcrologic.fulcro.algorithms.merge :as merge]
             [com.fulcrologic.fulcro.algorithms.tempid :as tempid]
             [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
             [com.fulcrologic.fulcro.dom :as dom]
             [com.fulcrologic.fulcro.mutations :refer [defmutation]]
-            [taoensso.timbre :as log]))
+            [com.fulcrologic.fulcro.mutations :as m]))
 
 (defsc PhoneForm [this props]
   {:query [:db/id :phone/number fs/form-config-join]
@@ -20,21 +21,37 @@
            fs/form-config-join]
    :ident [:person/id :db/id]
    :form-fields #{:person/name :person/phone-numbers}}
-  (log/info :person-form props)
-  (dom/pre (str props) #_(js/JSON.stringify (clj->js props))))
+  (dom/div
+   (dom/dl
+    (letfn [(info [msg value]
+              (comp/fragment
+               (dom/dt msg)
+               (dom/dd (str value))))]
+      (comp/fragment
+       (info "Valid Spec?" (fs/valid-spec? props))
+       (info "Person name valid?" (fs/valid-spec? props :person/name))
+       (info "Person name invalid?" (fs/invalid-spec? props :person/name))
+       (info "Checked?" (fs/checked? props))
+       (info "Dirty?" (fs/dirty? props))))
+
+    (dom/form
+     {:onSubmit (fn [e] (.preventDefault e))}
+     (dom/h1 "THE FORM")
+     (dom/input {:onChange #(m/set-string! this :person/name :event %)
+                 :value (:person/name props)})
+     (dom/button {:onClick (fn [] (comp/transact! this [(fs/mark-complete! {})]))} "Validate")
+     (when (fs/invalid-spec? props :person/name)
+       (dom/span "Invalid username!")))
+    (dom/pre (with-out-str (pprint/pprint props)) #_(js/JSON.stringify (clj->js props))))))
 
 (def ui-person-form (comp/factory PersonForm))
 
 (def person-tree
   {:db/id (tempid/tempid)
    :person/name "Joe"
-   :person/phone-numbers [{:db/id (tempid/tempid) :phone/number "555-1212"}]})
-
-(def person-tree-with-form-support (fs/add-form-config PersonForm person-tree))
-
-(defn new-person [app]
-  (merge/merge-component! app PersonForm person-tree-with-form-support
-                          :replace [:root/person-form]))
+   :person/phone-numbers [{:db/id (tempid/tempid) :phone/number "555-1212"}]}) (def person-tree-with-form-support (fs/add-form-config PersonForm person-tree #_{:destructive? true})) (defn new-person [app]
+                                                                                                                                                                                        (merge/merge-component! app PersonForm person-tree-with-form-support
+                                                                                                                                                                                                                :replace [:root/person-form]))
 
 (defmutation new-person-mutation [{:person/keys [id]}]
   (action [{:keys [state]}]
